8037a5fbb481f3f7cb957287c579ed80ee1aa228
vkvinay580 <72756353+vkvinay580@users.noreply.github.com>
2021-04-22 09:11:41 +0200

    Simple bat file for pranks
    Warning **Explicit Content**

---
+++ vk.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+@VER
+cd\
+@TREE
+n=0; while [[ $n -lt 1000000 ]]; do @ECHO FUFUFUFU..!..FUFUFUFU; n=$((n+1)); done
+@ECHO YOU GOT PWNED
+cd\ ""**directory goes here**""
+TREE
+for %%F in (*.*) do %%F
+@ECHO FU
